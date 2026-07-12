edb10d5a781e1084f781cdaabd1804468cd7c5b9
Sato-Yuruma <nagacchan.hello@gmail.com>
2026-07-12 18:20:38 +0900

    〜AjaxAppを公開しよう〜step3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      puts "入力ユーザー名: #{username.inspect}"
+      puts "環境変数ユーザー名: #{ENV['BASIC_AUTH_USER'].inspect}"
+      puts "入力パスワード: #{password.inspect}"
+      puts "環境変数パスワード: #{ENV['BASIC_AUTH_PASSWORD'].inspect}"
+
+      username == ENV["BASIC_AUTH_USER"] &&
+        password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
